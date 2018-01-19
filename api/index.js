d0c8d2c86fe16c9d173f58ff37b4fd02fc0ccafd
Szymon Kaliski <hi@szymonkaliski.com>
2018-01-19 12:46:38 +0100

    working rabbitmq

--- api/index.js
+++ api/index.js
@@ -1,10 +1,44 @@
 const express = require("express");
 const mongojs = require("mongojs");
+const amqp = require("amqplib/callback_api");
 
 const app = express();
+
+// rabbitmq
+amqp.connect("amqp://rabbitmq", (err, conn) => {
+  if (err) {
+    // exit if we can't connect to rabbitmq
+    // docker-compose will pick this up and restart
+    console.log(err);
+    process.exit(1);
+  } else {
+    console.log("RabbitMQ connected");
+
+    conn.createChannel((err, ch) => {
+      if (err) {
+        console.log(err);
+      } else {
+        const queue = "test-queue";
+
+        ch.assertQueue(queue, { durable: false });
+        ch.sendToQueue(queue, Buffer.from("Hello World!"));
+
+        console.log("sent message!");
+      }
+    });
+  }
+});
+
+// db
 const db = mongojs("mongodb://db:27017", ["data", "healthCheck"]);
 
-db.on("error", () => console.log("couldn't connect to DB"));
+db.on("error", err => {
+  // exit if we can't connect to db
+  // docker-compose will pick this up and restart
+  console.log("couldn't connect to DB", err);
+  process.exit(1);
+});
+
 db.on("connect", () => console.log("DB connection works"));
 
 // db health check
@@ -12,14 +46,11 @@ db.healthCheck.remove({ works: true }, err => {
   if (err) {
     console.log(err);
   } else {
-    db.healthCheck.insert(
-      { works: true, ts: new Date().getTime() },
-      err => {
-        if (err) {
-          console.log(err);
-        }
+    db.healthCheck.insert({ works: true, ts: new Date().getTime() }, err => {
+      if (err) {
+        console.log(err);
       }
-    );
+    });
   }
 });
 
