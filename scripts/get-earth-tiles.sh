23dd1cd30c0580ce3c7a5fc14164e597f74ffb4e
Szymon Kaliski <hi@szymonkaliski.com>
2018-01-30 10:21:25 +0100

    script for getting whole planet tiles

---
+++ scripts/get-earth-tiles.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+wget https://s3.amazonaws.com/mapbox/osm-qa-tiles-production/latest.planet.mbtiles.gz
+gunzip latest.planet.mbtiles.gz
+mkdir -p api/tiles/
+mv planet.mbtiles api/tiles/tiles.mbtiles
--- scripts/get-indonesia-tiles.sh
+++ scripts/get-indonesia-tiles.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 wget https://s3.amazonaws.com/mapbox/osm-qa-tiles-production/latest.country/indonesia.mbtiles.gz
-gunzip *.gz
+gunzip indonesia.mbtiles.gz
 mkdir -p api/tiles/
 mv indonesia.mbtiles api/tiles/tiles.mbtiles
