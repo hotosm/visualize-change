55416c34e12931ac9a6bce3248a0c7748a2a8965
Szymon Kaliski <hi@szymonkaliski.com>
2018-02-02 19:56:58 +0100

    cron script updates

--- tile-processor/get-and-process-tiles.sh
+++ tile-processor/get-and-process-tiles.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-wget https://s3.amazonaws.com/mapbox/osm-qa-tiles-production/latest.planet.mbtiles.gz
-gunzip latest.planet.mbtiles.gz
+wget -c https://s3.amazonaws.com/mapbox/osm-qa-tiles-production/latest.planet.mbtiles.gz
+mv latest.planet.mbtiles.gz /data/tiles
 
 # /data/tiles is shared volume with api component
-mv planet.mbtiles /data/tiles/tiles.mbtiles
+cd /data/tiles || exit 1
+gunzip latest.planet.mbtiles.gz
+mv planet.tiles tmp.tiles
 
-tippecanoe-decode tiles.mbtiles | tippecanoe -f -pf -pk -ps -b0 --no-tile-stats -l osm -Z0 -z12 -o tiles.processed.mbtiles
+tippecanoe-decode tmp.mbtiles | tippecanoe -f -pf -pk -ps -b0 --no-tile-stats -l osm -Z0 -z12 -o tiles.processed.mbtiles
 
 rm tiles.mbtiles
+rm tmp.mbtiles
 mv tiles.processed.mbtiles tiles.mbtiles
 
