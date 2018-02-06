3b49f6acc1ca189f10a10de93eb5622f6d374b16
Szymon Kaliski <hi@szymonkaliski.com>
2018-02-06 14:31:53 +0100

    mv tiles just outside of app folder for building

--- build.prod.sh
+++ build.prod.sh
@@ -4,8 +4,8 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$DIR" || exit 1
 
 # move data tiles out of shared volume for faster build
-mv docker/data/tiles/tiles.mbtiles /tmp/
+mv docker/data/tiles/tiles.mbtiles ../
 
 docker-compose -f docker/docker-compose.prod.yml build
 
-mv /tmp/tiles.mbtiles docker/data/tiles/
+mv ../tiles.mbtiles docker/data/tiles/
