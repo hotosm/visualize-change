ff8a14247a7a397b20247fb650079ccdfd1547ae
Szymon Kaliski <hi@szymonkaliski.com>
2018-02-06 13:15:07 +0100

    more script fixes

--- scripts/get-earth-tiles.sh
+++ scripts/get-earth-tiles.sh
@@ -5,7 +5,8 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # docker/data/tiles is shared volume with api component
 cd "$DIR"/../docker/data/tiles || exit 1
 
+rm latest.planet.mbtiles.gz
 wget https://s3.amazonaws.com/mapbox/osm-qa-tiles-production/latest.planet.mbtiles.gz
 gunzip latest.planet.mbtiles.gz
 
-mv latest.planet.mbtiles docker/data/tiles/tiles.mbtiles
+mv latest.planet.mbtiles tiles.mbtiles
--- scripts/get-indonesia-tiles.sh
+++ scripts/get-indonesia-tiles.sh
@@ -5,7 +5,8 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # docker/data/tiles is shared volume with api component
 cd "$DIR"/../docker/data/tiles || exit 1
 
+rm indonesia.mbtiles.gz
 wget https://s3.amazonaws.com/mapbox/osm-qa-tiles-production/latest.country/indonesia.mbtiles.gz
 gunzip indonesia.mbtiles.gz
 
-mv indonesia.mbtiles docker/data/tiles/tiles.mbtiles
+mv indonesia.mbtiles tiles.mbtiles
