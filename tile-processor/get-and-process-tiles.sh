0191091721091ad5dba742d41ca7f3822c91e6c9
Szymon Kaliski <hi@szymonkaliski.com>
2018-02-09 17:59:26 +0100

    tile-processor updates

--- tile-processor/get-and-process-tiles.sh
+++ tile-processor/get-and-process-tiles.sh
@@ -7,7 +7,7 @@ wget -c https://s3.amazonaws.com/mapbox/osm-qa-tiles-production/latest.planet.mb
 gunzip -f latest.planet.mbtiles.gz
 
 if [ "$GENERATE_UNDERZOOM" = "1" ]; then
-  mv -f latest.planet.tiles tmp.mbtiles
+  mv -f latest.planet.mbtiles tmp.mbtiles
 
   tippecanoe-decode tmp.mbtiles | tippecanoe -f -pf -pk -ps -b0 --no-tile-stats -l osm -Z0 -z12 -o tiles.processed.mbtiles
 
@@ -15,6 +15,6 @@ if [ "$GENERATE_UNDERZOOM" = "1" ]; then
 
   mv -f tiles.processed.mbtiles tiles.mbtiles
 else
-  mv -f latest.planet.tiles tiles.mbtiles
+  mv -f latest.planet.mbtiles tiles.mbtiles
 fi
 
