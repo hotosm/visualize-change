33a58b4c315816f30538364c080f5366a50cdffe
Szymon Kaliski <hi@szymonkaliski.com>
2018-02-16 12:46:36 +0100

    testing husky + lint-staged

--- scripts/start.dev.sh
+++ scripts/start.dev.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-cd "$DIR" || exit 1
+cd "$DIR/.." || exit 1
 
-docker-compose -f docker/docker-compose.prod.yml up
+docker-compose -f docker/docker-compose.dev.yml up
