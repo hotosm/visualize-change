4dd3323d33dab45b0030e6f9d387a80a2a00881b
Szymon Kaliski <hi@szymonkaliski.com>
2018-03-02 13:02:49 +0100

    basic sidebar

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,12 +9,20 @@ const {
   Navbar,
   NavbarGroup,
   NavbarHeading,
-  Popover
+  Popover,
+  Tab,
+  Tabs,
+  Label
 } = require("@blueprintjs/core");
+const { DateRangePicker } = require("@blueprintjs/datetime");
 
 require("normalize.css/normalize.css");
 require("mapbox-gl/dist/mapbox-gl.css");
 require("@blueprintjs/core/lib/css/blueprint.css");
+require("@blueprintjs/icons/lib/css/blueprint-icons.css");
+require("@blueprintjs/datetime/lib/css/blueprint-datetime.css");
+
+require("./style.less");
 
 // mapboxgl.accessToken = process.env.MAPBOX_ACCESS_TOKEN;
 // const IS_DEBUG = window.location.href.indexOf("debug") > 0;
@@ -26,10 +34,48 @@ const LanguageMenu = () => (
   </Menu>
 );
 
+const Main = ({ children }) => <div className="main">{children}</div>;
+
+const DescribePanel = () => (
+  <div>
+    <Label text="Name" required={true}>
+      <input className="pt-input" />
+    </Label>
+    <Label text="Description" required={true}>
+      <textarea className="pt-input" />
+    </Label>
+    <Label text="Project" required={true}>
+      <input className="pt-input" />
+    </Label>
+  </div>
+);
+
+const DatePanel = () => <DateRangePicker shortcuts={false} contiguousCalendarMonths={false} maxDate={new Date()} />;
+
+const StylePanel = () => <div>TODO</div>;
+
+const Sidebar = () => (
+  <div className="sidebar">
+    <div className="sidebar-content">
+      <Tabs animate={true} id="SidebarTabs" renderActiveTabPanelOnly={true}>
+        <Tab id="Describe" title="Describe" panel={<DescribePanel />} />
+        <Tab id="Date" title="Date" panel={<DatePanel />} />
+        <Tab id="Style" title="Style" panel={<StylePanel />} />
+      </Tabs>
+    </div>
+
+    <div className="sidebar-footer">
+      <div className="sidebar-footer__content">Made with love by HOT and friends</div>
+    </div>
+  </div>
+);
+
+const Map = () => <div className="map">MAP</div>;
+
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className="container">
         <Navbar>
           <NavbarGroup>
             <NavbarHeading>HOT Visualize Change</NavbarHeading>
@@ -47,6 +93,11 @@ class App extends React.Component {
             </ButtonGroup>
           </NavbarGroup>
         </Navbar>
+
+        <Main>
+          <Sidebar />
+          <Map />
+        </Main>
       </div>
     );
   }
--- frontend/webpack.common.js
+++ frontend/webpack.common.js
@@ -25,6 +25,14 @@ module.exports = {
         use: ["style-loader", "css-loader"]
       },
       {
+        test: /\.less$/,
+        use: ["style-loader", "css-loader", "less-loader"]
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: "url-loader"
+      },
+      {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: "babel-loader",
