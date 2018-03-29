fa139aa6079bb2993d26eae739a11a8fc765e3a7
Szymon Kaliski <hi@szymonkaliski.com>
2018-03-29 13:47:16 +0200

    link to 'new' visualization, and more pedantic eslint

--- frontend/src/components/topbar.js
+++ frontend/src/components/topbar.js
@@ -1,39 +1,19 @@
 const React = require("react");
 const classNames = require("classnames");
-const {
-  AnchorButton,
-  Button,
-  ButtonGroup,
-  Navbar,
-  NavbarGroup,
-  NavbarHeading,
-  Popover,
-  Menu,
-  MenuItem,
-  Intent
-} = require("@blueprintjs/core");
+const { Button, ButtonGroup, Navbar, NavbarGroup, NavbarHeading, Intent } = require("@blueprintjs/core");
 
 const { SlideTransition } = require("./transitions");
 const { Link } = require("react-router-dom");
 
-const LanguageMenu = () => (
-  <Menu>
-    <MenuItem text="English" />
-    <MenuItem text="Polish" />
-  </Menu>
-);
+// const LanguageMenu = () => (
+//   <Menu>
+//     <MenuItem text="English" />
+//     <MenuItem text="Polish" />
+//   </Menu>
+// );
 
 // TODO: add defaults
-module.exports = ({
-  id = null,
-  path,
-  canSave,
-  saving,
-  isEditing,
-  onSaveClick,
-  onToggleViewState,
-  isFullScreenMode
-}) => (
+module.exports = ({ id = null, path, canSave, saving, isEditing, onSaveClick, isFullScreenMode }) => (
   <SlideTransition className="topbar" visible={!isFullScreenMode} direction="top">
     <Navbar>
       <NavbarGroup>
@@ -52,21 +32,28 @@ module.exports = ({
             </Button>
           )}
 
+          <Link className="pt-button route" to="/edit/">
+            New
+          </Link>
+
           <Link
             className={classNames("pt-button route", { active: path === "view" })}
             to={`/view${id ? "/" + id : ""}`}
           >
             View
           </Link>
+
           <Link
             className={classNames("pt-button route", { active: path === "edit" })}
             to={`/edit${id ? "/" + id : ""}`}
           >
             Edit
           </Link>
+
           <Link className={classNames("pt-button route", { active: path === "about" })} to="/">
             About
           </Link>
+
           <Link className={classNames("pt-button route", { active: path === "learn" })} to="/learn">
             Learn
           </Link>
