a9d904457d92120b4f7f2a30bc864c57ec9c5a9b
Szymon Kaliski <hi@szymonkaliski.com>
2018-04-16 11:03:35 +0200

    re-added tile minzooms

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -7,11 +7,12 @@ const { routerMiddleware } = require("react-router-redux");
 const reducers = require("./reducers/index");
 
 const IS_PRODUCTION = process.env.NODE_ENV === "production";
+const IS_NO_LOGGER = document.location.search.indexOf("no-logger") >= 0;
 
 module.exports = ({ history }) => {
   let middleware;
 
-  if (IS_PRODUCTION) {
+  if (IS_PRODUCTION || IS_NO_LOGGER) {
     middleware = applyMiddleware(thunk, routerMiddleware(history));
   } else {
     middleware = applyMiddleware(thunk, createLogger(), routerMiddleware(history));
