43c030f6989936df5b4d8db1f27ea3a40cd22a12
Szymon Kaliski <hi@szymonkaliski.com>
2018-04-18 19:13:07 +0200

    mapbox-gl compatible dist build

--- frontend/webpack.prod.js
+++ frontend/webpack.prod.js
@@ -1,17 +1,28 @@
-// const MinifyPlugin = require('babel-minify-webpack-plugin');
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 const config = require("./webpack.common");
 const webpack = require("webpack");
 
 module.exports = Object.assign({}, config, {
+  module: {
+    ...config.module,
+
+    // uglifyjs breaks mapbox
+    noParse: /(mapbox-gl)\.js$/
+  },
+
   plugins: [
     ...config.plugins,
 
     new webpack.LoaderOptionsPlugin({
       minimize: true,
       debug: false
-    })
+    }),
 
-    // FIXME: using MinifyPlugin ends in "k is not defined" error
-    // new MinifyPlugin({ removeConsole: true, removeDebugger: true }, { comments: false })
+    new UglifyJsPlugin({
+      parallel: true,
+      uglifyOptions: {
+        compress: { inline: false }
+      }
+    })
   ]
 });
