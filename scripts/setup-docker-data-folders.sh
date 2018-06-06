37afd37cd1b51da19149b2738bf73167f2a9a744
Mhairi O'Hara <mhairi.ohara@hotosm.org>
2018-06-06 10:02:08 +0800

    Docker folder is in the current working space

--- scripts/setup-docker-data-folders.sh
+++ scripts/setup-docker-data-folders.sh
@@ -3,7 +3,7 @@
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$DIR/.." || exit 1
 
-cd ../docker/ || exit 1
+cd docker/ || exit 1
 
 mkdir -p data/capture data/db data/rabbitmq data/tiles > /dev/null
 
