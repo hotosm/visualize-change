7d3730841c4471eed2d4200679a47c8774922b14
Dakota Benjamin <dakota.benjamin@gmail.com>
2018-07-19 12:16:55 -0400

    change environment

--- .circleci/eb_deploy.sh
+++ .circleci/eb_deploy.sh
@@ -7,7 +7,7 @@ DEV_BRANCH="develop"
 PROD_BRANCH="master"
 
 # DEMO_ENV="visualize-change-demo"
-DEV_ENV="visualize-change-dev-2"
+DEV_ENV="VisualizeChange-env-1"
 PROD_ENV="visualize-change-prod"
 
 echo Running HOT Visualize Change Deployment, current branch is $CIRCLE_BRANCH
